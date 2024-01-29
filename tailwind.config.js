5aa75b455658b65a3b49848bbd2689b1c73ab5b3
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2024-01-29 10:19:49 +0100

    Sections by NiengeDavid
    I built my sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./node_modules/flowbite/**/*.js"],
+
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
